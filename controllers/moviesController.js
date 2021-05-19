f5b29caee8315d45e48716051867ee1a5cee05c6
guillermo <guillaumeraez@gmail.com>
2021-05-19 16:21:52 +0200

    Added the link with the variable to identify the names of the movies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,10 +12,10 @@ class Movies {
         return res.data;
     }
 
-    // async findMoviesByTitle(titulo) {
-    //     // let res = await axios.get('');
-    //     return res.data;
-    // }
+    async findMoviesByTitle(titulo) {
+        let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&query=${titulo}`);
+        return res.data;
+    }
 }
 
 let moviesController = new Movies();
