4e9d8c42d9e260f3f4471bd1ac2e02064a74c689
guillermo <guillaumeraez@gmail.com>
2021-05-19 13:13:38 +0200

    Modified index.js with the main values

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = required('express');
+const app = express();
+const port = 3000;
+
+const router = require('./router');
+
+//Middleware
+app.use(express.json());
+app.use(router);
+
+
+app.listen(port, () => console.log(`Node server up and running at http://localhost:${port}`));
