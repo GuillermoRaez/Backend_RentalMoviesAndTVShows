e58de0d4f5b1d9412b7407e4a48e2f0e83ca1ed5
AdrianaFayos <adrifayosperis@gmail.com>
2021-05-24 18:09:04 +0200

    added users endpoint

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
-
 //Verificar siempre que esta en comentario cuando el proyecto no esta completo.
 const moviesRouter = require('./routes/moviesRouter');
 const tvshowsRouter = require('./routes/tvshowsRouter');
+const userRouter = require('./routes/userRouter');
 
+router.use('/users', userRouter);
 router.use('/movies', moviesRouter);
 router.use('/tvshows', tvshowsRouter);
 
