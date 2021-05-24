22ce16f71628bb43fe67a01a78f6eae77e705ca7
guillermo <guillaumeraez@gmail.com>
2021-05-24 17:29:48 +0200

    added the db const to the file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const db = require('./db.js');
 const router = require('./router');
 
 //Middleware
 app.use(express.json());
 app.use(router);
 
+db
+.then(()=>{
 
-app.listen(port, () => console.log(`Node server up and running at http://localhost:${port}`));
+    app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
+})
+.catch((err)=> console.log(err.message));
