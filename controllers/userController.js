f43fd9458e78a5e02a6c818d31f7541bbec848ae
guillermo <guillaumeraez@gmail.com>
2021-05-25 17:51:49 +0200

    Created the userController for the CRUD functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,23 @@ class Client {
         return User.create(client);
     }
 
+    async modifyUser(bodyData){
+        return User.update(
+            //Datos que cambiamos..
+            {email: bodyData.email, adress: bodyData.adress},
+            //Donde...
+            {where: {id: bodyData.id}}
+        )
+    }
+
+
+    async deleteUser(id){
+
+        return User.destroy({where: {id:id}});
+    }
 }
 
 
+
 let userController = new Client();
-module.exports = userController;
+module.exports = userController; 
