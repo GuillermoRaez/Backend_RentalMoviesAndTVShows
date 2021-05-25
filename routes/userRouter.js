7ba406971885a77e428bb66889a3d31229b44e22
guillermo <guillaumeraez@gmail.com>
2021-05-25 17:52:42 +0200

    Created the endpoints for user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
+// Endpoint de Perfil (R) -> GET
 router.get('/', async(req, res) => {
     try {
         res.json(await userController.findAllUsers())
@@ -11,11 +12,36 @@ router.get('/', async(req, res) => {
     }
 });
 
+// Endpoint de Alta de Usuario (C) -> POST
 router.post('/', async(req, res) => {
     try {
         const id = await userController.signUpUser(req.body);
         const status = 'success';
         res.json({ status, id });
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+})
+
+// Endpoint de Modificación de datos (U) -> UPDATE
+router.put('/', async (req,res) => {
+    try{
+        const bodyData = req.body;
+        res.json(await userController.modifyUser(bodyData)); 
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+})
+
+// Endpoint de Baja de Usuario (D) -> Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        res.json(await userController.deleteUser(id))
     } catch (err) {
         return res.status(500).json({
             message: err.message
