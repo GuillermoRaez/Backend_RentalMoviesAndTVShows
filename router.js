ff1c5bf971515686ae9f904de84f7ba2294a84ce
guillermo <guillaumeraez@gmail.com>
2021-05-27 13:10:21 +0200

    Added the order endpoints to router

--- router.js
+++ router.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const moviesRouter = require('./routes/moviesRouter');
 const tvshowsRouter = require('./routes/tvshowsRouter');
 const userRouter = require('./routes/userRouter');
-const loginRouter = require('./routes/loginRouter')
+const loginRouter = require('./routes/loginRouter');
+const orderRouter = require('./routes/orderRouter');
 
 router.use('/users', userRouter);
+router.use('/orders', orderRouter);
 router.use('/movies', moviesRouter);
 router.use('/tvshows', tvshowsRouter);
 router.use('/login', loginRouter);
