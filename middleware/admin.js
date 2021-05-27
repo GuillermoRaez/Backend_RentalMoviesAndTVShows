3221e3b28df16e18fae611850d83993f1814b621
AdrianaFayos <adrifayosperis@gmail.com>
2021-05-27 16:48:54 +0200

    admin middleware created

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,22 +1,21 @@
 const jwt = require('jsonwebtoken');
 const secret = "Guillermo & Adriana Rental Movies";
 
-//COMPROBAR EL TOKEN
-
-const authenticate = (req,res,next) => {
+const admin = (req,res,next) => {
 
     try {
 
         if(!req.headers.authorization){
-            return "no tenías token "; 
+            return "You don't have any token.";
+            
         }
 
         let token = req.headers.authorization.split(' ')[1];
 
         let auth = jwt.verify(token,secret);
 
-        if(auth.userId != req.params.id){
-            throw new Error ("No tienes permiso para realizar esta acción");
+        if(auth.isAdmin == false){
+            throw new Error("You don't have authorization.");
         }
 
         return next();
@@ -26,7 +25,7 @@ const authenticate = (req,res,next) => {
             message: error.message
         });
     }
-    
+
 }
 
-module.exports = authenticate;
+module.exports = admin;
